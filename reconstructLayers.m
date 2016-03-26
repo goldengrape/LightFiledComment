25947c4fbfd02509de62f019aded6a63774d2cc4
GoldenGrape <goldengrape@goldengrape.org>
2016-03-26 16:06:21 +0800

    从reconstruct layers开始注释
    这是quick start的主程序

--- reconstructLayers.m
+++ reconstructLayers.m
@@ -1,3 +1,4 @@
+% 这是readme.txt中所述的快速起始运行程序
 clear all;
 global T;
 
@@ -6,6 +7,9 @@ global T;
 % user parameters
 
 % number of layers
+% 用户参数，定义layers的层数和最大迭代次数。
+% 我不确定layers是指的观察角度还是说指的用来做光场显示的显示器层数。
+% 好像layers是指的用来做光场显示的显示器层数
 numLayers                           = 5;
 
 % max iterations for optimization
@@ -15,11 +19,18 @@ maxIters                            = 15;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
  
 % datapath to light field
+% 数据所用的路径，用来产生光场数据的并不是从一个虚拟3D物体用光路追迹算出来的。
+% 而是使用了用相机从不同方向上对同一个物体拍照获得的图像数据。
 datapath = 'data/';
 
 % load light field
+% 光场的数据需要实现使用data目录下的generateLightField.m子程序来计算，
+% 计算的结果会保存在LightField4D.mat文件中，文件有45M，所以我不会保存在github上。
 lightFieldFilename = [datapath 'LightField4D.mat'];
 
+% 如果光场数据文件还没有产生，就报错。
+% 此时需要运行data目录下的generateLightField.m子程序来计算，
+% 如果有光场数据文件，就调用之。
 if ~exist(lightFieldFilename, 'file')
 	error('No light field given and the generic file does not exist!');
 end
@@ -27,7 +38,15 @@ end
 % load everything
 load(lightFieldFilename);                
     
-    
+% 在LightField4D.mat数据文件中保存着：
+% lightField--这是一个5D的数组，大小是(7,7,384,512,3)，也就是7*7个视角的512*384彩色图像
+% lightFieldAnglesX
+% lightFieldAnglesY
+% lightFieldOrigin
+% lightFieldResolution=[7,7,7,384,512,3]
+% lightFieldSize=[75,100]
+
+ 
 % set layer resolution here
 layerResolution = [lightFieldResolution(3) lightFieldResolution(4) numLayers];
 
